96ecdeec3a224cf630d17153e8ca7b9c74ab999b
lingg <kanladaporn.sir@gmail.com>
2024-10-12 23:30:07 +0700

    Add MinimumGroups.java

---
+++ java/src/main/java/medium/MinimumGroups.java
@@ -0,0 +1,27 @@
+package medium;
+
+import java.util.*;
+
+// https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/?envType=daily-question&envId=2024-10-12
+public class MinimumGroups {
+    public int minGroups(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
+
+        PriorityQueue<Integer> groups = new PriorityQueue<>();
+
+        for (int[] interval : intervals) {
+            if (!groups.isEmpty() && interval[0] > groups.peek()) {
+                groups.poll();
+            }
+            groups.offer(interval[1]);
+        }
+        System.out.println(groups);
+        return groups.size();
+    }
+
+    public static void main(String[] args) {
+        MinimumGroups solution = new MinimumGroups();
+        System.out.println(solution.minGroups(new int[][]{{5, 10}, {6, 8}, {1, 5}, {2, 3}, {1, 10}})); // 3
+        System.out.println(solution.minGroups(new int[][]{{1, 3}, {5, 6}, {8, 10}, {11, 13}})); // 1
+    }
+}
